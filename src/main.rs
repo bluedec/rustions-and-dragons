7ae5db8de09ab80117a90def32bfd691ae704711
bluedec <yisussaez@gmail.com>
2023-04-08 04:53:09 -0300

    Added crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::thread;
 use std::time::Duration; 
 use magic::Class;
-use std::io;
+use std::io::{Write, stdout, stdin};
+use crossterm::{
+    event, QueueableCommand, cursor, ExecutableCommand, queue
+};
+use crossterm::cursor::{ Show, Hide };
+use crossterm::{ execute, terminal::{ Clear, ClearType } };
 
 pub fn take_inp() -> String {
     let mut input2 = String::new();
-    io::stdin()
+    stdin()
         .read_line(&mut input2)
         .expect("Failed to read input.");
 
@@ -13,10 +18,37 @@ pub fn take_inp() -> String {
 }
 
 fn main() {
-    println!("<<<0/ O \\0>>>Welcome to Rustions and Dragons");        
+    let dragon = "                                                                      
+                                 d$                                          
+                               .$$F                                          
+                              z$$$                             d$$b     .$$  
+-__                          J$$$$                            d$. $$$$$$$$$  
+  \\                         4$$$$$                           z$$$$$$$$$$$$\"  
+   \"c                       $$$$$$F                         d$$$*$$$$\"       
+    \"$e.      .            $$$$$$$$                       .$$$$$             
+     3$$$$$$P\"             $$$$$$$$c                    .$$$$$$              
+      $$$$$\"               $$$$$$$$$r                .e$$$$$$$F              
+      *$$$b                $$$$$$$$$$c           .e$$$$$$$$$$P               
+      '$P \"$.              $$$$$$$$$$$b.    .ee$$$$$$$$$$$$$P                
+       $   ^$$.             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$F                 
+       \"     $$$$e..      .e$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"                  
+             \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P                    
+                *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P                      
+                  \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*                        
+                     \"*$$$$$$$$$$$$$$$$$$$$$$$$$$$\"                          
+                         \"*$$$$$$$$$$$$$$$$$$$$$\"                            
+                             *$$$$$$$$$$$$$$$\"                               
+                                  \"\"\"\"\"\"\"\"\"       Gilo94'";
+    println!("{}", dragon);
+    let mut stdout = stdout();
+    println!("\n<<<0/ O \\0>>>Welcome to Rustions and Dragons");        
     println!("1) Start");
-    let inp = take_inp();
-    if inp ==
+    execute!(stdout, Hide).unwrap();
+    stdout.queue(cursor::MoveTo(65, 28)).unwrap();
+    
+    execute!(stdout, Clear(ClearType::CurrentLine)).unwrap();
+    println!("Your cursor here.");
+    stdout.flush();
 }
 
 
