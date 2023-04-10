4b992be1bc935a55e0d45e6fb4028b6eb49a5655
bluedec <yisussaez@gmail.com>
2023-04-10 05:03:56 -0300

    Polished

--- magic/src/lib.rs
+++ magic/src/lib.rs
@@ -1,46 +1,100 @@
 use std::thread;
 use std::time::Duration;
+use std::io;
+
+use std::sync::mpsc;
+use std::sync::{ Arc, Mutex };
+
+use crossterm::event::Event;
+use crossterm::{ event, cursor, terminal, execute };
+use crossterm::terminal::{ enable_raw_mode, disable_raw_mode, Clear, ClearType };
 
 pub enum Choice {
+    Go,
     Next,
     Prev,
     Break,
 }
 
 pub struct Human {
-    name: String,
-    health: i32,
-    class: Class,
+    pub name: String,
+    pub health: i32,
+    pub class: Class,
 }
 pub struct Class {
-    name: String,
-    level: i32,
+    pub name: String,
+    pub level: i32,
 }
 pub struct Weapon {
-    name: String,
-    level: i32,  
-    damage: i32,
-    class: Class,
-    min_lvl: i32, 
+    pub name: String,
+    pub level: i32,  
+    pub damage: i32,
+    pub class: Class,
+    pub min_lvl: i32, 
 }
 
+pub struct Wing {
+    pub name: String,
+    pub weight: usize,
+    pub habilities: Vec<String>,
+}
 
 pub fn wait_a_sec() {
     thread::sleep(Duration::from_secs(1));
 }
 
-pub fn show_options(options: &Vec<&str>, selected_option: i32, title: &str) {
-    println!("{}", title);
-    for (i, option) in options.iter().enumerate() {
-        if i == selected_option.try_into().unwrap() {
-            println!("{} <\r", option)
-        } else {
-            println!("{}\r", option);
+pub fn zone(options: &Vec<&str>, selected_option: i32, title: &str, tx: &Arc<Mutex<mpsc::Sender<Choice>>>) {
 
+    let max_option_range = options.len() - 1;
+    let mut so = selected_option.clone();
+    let n = options.len() + 1;
+
+    loop {
+
+        println!(" {}\r", title);
+
+        for (i, option) in options.iter().enumerate() {
+            if i == so as usize {
+                println!("{} <\r", option)
+            } else {
+                println!("{}\r", option);
+
+            }
+        }
+
+        let event = event::read().unwrap();
+
+        if event == Event::Key(event::KeyCode::Up.into()) {
+            execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(n as u16)).unwrap(); 
+            if so > 0 {
+                so -= 1;
+                continue
+            }
+        }
+
+        if event == Event::Key(event::KeyCode::Enter.into()) {
+            execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(n as u16)).unwrap();
+            tx.lock().unwrap().send(Choice::Go).unwrap();
+        }
+
+        if event == Event::Key(event::KeyCode::Down.into()) {
+            execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(n as u16)).unwrap();
+            if so < max_option_range as i32 {
+                so += 1;
+                continue
+            }                     
+        }
+
+        if event == Event::Key(event::KeyCode::Esc.into()) {
+            // might add a pop up screen to confirm the exit
+            terminal::Clear(ClearType::FromCursorUp);
+            println!("Exiting the listen...\r");
+            wait_a_sec();
+            //tx.send(Choice::Break).expect("Failed to send Choice::Break");
+            break
         }
     }
 }
-
 pub fn take_inp() -> String {
     let mut input2 = String::new();
     std::io::stdin()
@@ -77,13 +131,3 @@ pub fn dragon_intro() {
     println!("\nWelcome to Rustions and Dragons\r");        
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
-use std::io::{Write, stdout };
+use std::io::{ Write, stdout };
+
+use std::sync::{ Arc, Mutex };
 
 use std::sync::mpsc;
 
@@ -8,89 +10,45 @@ use crossterm::{ event, cursor, terminal, execute };
 use crossterm::terminal::{ enable_raw_mode, disable_raw_mode, Clear, ClearType };
 
 use magic::{ wait_a_sec, Choice };
-use magic:: show_options;
+use magic::{ zone, Wing};
 
 
 
 fn main() -> io::Result<()> {
     let mut stdout = stdout();
-    execute!(
-        stdout,
-        cursor::Hide,
-        cursor::MoveTo(0, 0),
-        Clear(ClearType::CurrentLine),
-    ).unwrap();
-
+    pub fn clean(stdout: &mut io::Stdout) {
+        execute!(
+            stdout,
+            cursor::Hide,
+            cursor::MoveTo(0, 0),
+            Clear(ClearType::All),
+            ).unwrap();
+    }
+    clean(&mut stdout);
+    println!("Enabling Raw Mode\r");
     enable_raw_mode()?;
 
     let options: Vec<&str> = vec![") Start", ") Quit"];
     let mut selected_option: i32 = 0;
-    let max_option_range = options.len() - 1;
 
-    pub fn zone(options: Vec<&str>, selected_option: i32, title: &str) {
-        'zone: loop {
-            show_options(&options, selected_option, title);
-            let event = event::read().unwrap();
-        }
-    }
     'master: loop {
-        execute!(stdout, Clear(ClearType::All)).unwrap();
         // transmiter for input
         let (tx, rx): (mpsc::Sender<Choice>, mpsc::Receiver<Choice>) = mpsc::channel();
 
-        show_options(&options, selected_option, "Main Menu\r");
-
-        // cloned for simplicity
-        let slc_opt_copy = selected_option.clone();
-        let max_option_range_copy = max_option_range.clone();
-
-        // thread made to receive input 
-        let _input_thread = std::thread::spawn(move || {
-            loop {
-                let event = event::read().unwrap();
-
-                if event == Event::Key(event::KeyCode::Up.into()) {
-                    if slc_opt_copy > 0 {
-                        execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(3)).unwrap();
-                        tx.send(Choice::Next).expect("Lol 1");
-                        break;
-                    }
-                }
-
-                if event == Event::Key(event::KeyCode::Enter.into()) {
-                    
-                }
-
-                if event == Event::Key(event::KeyCode::Down.into()) {
-                    if slc_opt_copy < max_option_range_copy.try_into().unwrap() {
-                        execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(3)).unwrap();
-                        tx.send(Choice::Prev).expect("Lol 2");
-                        break
-                    }                     
-                }
-
-                if event == Event::Key(event::KeyCode::Esc.into()) {
-                    // might add a pop up screen to confirm the exit
-                    terminal::Clear(ClearType::FromCursorUp);
-                    println!("Exiting the listen...\r");
-                    wait_a_sec();
-                    tx.send(Choice::Break).expect("Failed to send Choice::Break");
-                    break
-                }
-
-            }
-        });
+        let tx_arc: Arc<Mutex<mpsc::Sender<Choice>>> = Arc::new(Mutex::new(tx));
+        let tx_arc_clone = tx_arc.clone();
+        
+        zone(&options, 0, "Main Menu", &tx_arc_clone); 
 
         let data = rx.recv().unwrap();
 
         match data {
+            Choice::Go => println!("Go!"),
             Choice::Next => selected_option -= 1,
             Choice::Prev => selected_option += 1,
             Choice::Break => break 'master
 
         }
-
-
     }
     println!("Disabling Raw Mode\r");
 
@@ -101,3 +59,11 @@ fn main() -> io::Result<()> {
 }
 
 
+fn find_wing() {
+    println!("I'm trying to give you a wing here...");
+    let win = Wing {
+        name: String::from("WIN"),
+        weight: 6,
+        habilities: vec![String::from("Gust"), String::from("Heal")],
+    };
+}
