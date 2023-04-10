0f1c62e4e4123d7aa7c6ff6d93c9dcbf2b120d25
bluedec <yisussaez@gmail.com>
2023-04-09 22:21:26 -0300

    Cleanup, working menu

--- magic/src/lib.rs
+++ magic/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::thread;
+use std::time::Duration;
+
+pub enum Choice {
+    Next,
+    Prev,
+    Break,
 }
 
 pub struct Human {
@@ -11,13 +16,65 @@ pub struct Class {
     name: String,
     level: i32,
 }
-
 pub struct Weapon {
     name: String,
     level: i32,  
     damage: i32,
     class: Class,
     min_lvl: i32, 
+}
+
+
+pub fn wait_a_sec() {
+    thread::sleep(Duration::from_secs(1));
+}
+
+pub fn show_options(options: &Vec<&str>, selected_option: i32, title: &str) {
+    println!("{}", title);
+    for (i, option) in options.iter().enumerate() {
+        if i == selected_option.try_into().unwrap() {
+            println!("{} <\r", option)
+        } else {
+            println!("{}\r", option);
+
+        }
+    }
+}
+
+pub fn take_inp() -> String {
+    let mut input2 = String::new();
+    std::io::stdin()
+        .read_line(&mut input2)
+        .expect("Failed to read input.");
+
+    input2.trim().to_lowercase().to_string()
+}
+
+pub fn dragon_intro() {
+
+    let dragon = "                                                                      
+                                 d$                                          
+                               .$$F                                          
+                              z$$$                             d$$b     .$$  
+-__                          J$$$$                            d$. $$$$$$$$$  
+  \\                         4$$$$$                           z$$$$$$$$$$$$\"  
+   \"c                       $$$$$$F                         d$$$*$$$$\"       
+    \"$e.      .            $$$$$$$$                       .$$$$$\r               
+     3$$$$$$P\"             $$$$$$$$c                    .$$$$$$\r             
+      $$$$$\"               $$$$$$$$$r                .e$$$$$$$F\r              
+      *$$$b                $$$$$$$$$$c           .e$$$$$$$$$$P\r               
+      '$P \"$.              $$$$$$$$$$$b.    .ee$$$$$$$$$$$$$P\r                
+       $   ^$$.             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$F\r                 
+       \"     $$$$e..      .e$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"\r                  
+             \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P\r                    
+                *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P \r                      
+                  \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*\r                       
+                     \"*$$$$$$$$$$$$$$$$$$$$$$$$$$$\"\r                          
+                         \"*$$$$$$$$$$$$$$$$$$$$$\"\r                            
+                             *$$$$$$$$$$$$$$$\"\r                               
+                                  \"\"\"\"\"\"\"\"\"       Gilo94'";
+    println!("{}", dragon);
+    println!("\nWelcome to Rustions and Dragons\r");        
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,103 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration; 
 use std::io;
-use magic::Class;
-use std::io::{Write, stdout, stdin};
+use std::io::{Write, stdout };
+
+use std::sync::mpsc;
+
 use crossterm::event::Event;
-use crossterm::{
-    event, QueueableCommand, cursor, ExecutableCommand, queue
-};
-use crossterm::terminal;
-use crossterm::cursor::{ Show, Hide };
-use crossterm::{ execute, terminal::{ Clear, ClearType } };
+use crossterm::{ event, cursor, terminal, execute };
+use crossterm::terminal::{ enable_raw_mode, disable_raw_mode, Clear, ClearType };
 
-pub fn take_inp() -> String {
-    let mut input2 = String::new();
-    stdin()
-        .read_line(&mut input2)
-        .expect("Failed to read input.");
-
-    input2.trim().to_lowercase().to_string()
-}
+use magic::{ wait_a_sec, Choice };
+use magic:: show_options;
 
 
-fn main() {
+
+fn main() -> io::Result<()> {
     let mut stdout = stdout();
-    execute!(stdout, Hide).unwrap();
-    execute!(stdout, Clear(ClearType::All)).unwrap(); 
-    execute!(stdout, Clear(ClearType::CurrentLine)).unwrap();
-    let dragon = "                                                                      
-                                 d$                                          
-                               .$$F                                          
-                              z$$$                             d$$b     .$$  
--__                          J$$$$                            d$. $$$$$$$$$  
-  \\                         4$$$$$                           z$$$$$$$$$$$$\"  
-   \"c                       $$$$$$F                         d$$$*$$$$\"       
-    \"$e.      .            $$$$$$$$                       .$$$$$             
-     3$$$$$$P\"             $$$$$$$$c                    .$$$$$$              
-      $$$$$\"               $$$$$$$$$r                .e$$$$$$$F              
-      *$$$b                $$$$$$$$$$c           .e$$$$$$$$$$P               
-      '$P \"$.              $$$$$$$$$$$b.    .ee$$$$$$$$$$$$$P                
-       $   ^$$.             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$F                 
-       \"     $$$$e..      .e$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"                  
-             \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P                    
-                *$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P                      
-                  \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*                        
-                     \"*$$$$$$$$$$$$$$$$$$$$$$$$$$$\"                          
-                         \"*$$$$$$$$$$$$$$$$$$$$$\"                            
-                             *$$$$$$$$$$$$$$$\"                               
-                                  \"\"\"\"\"\"\"\"\"       Gilo94'";
-    println!("{}", dragon);
-    println!("\nWelcome to Rustions and Dragons");        
+    execute!(
+        stdout,
+        cursor::Hide,
+        cursor::MoveTo(0, 0),
+        Clear(ClearType::CurrentLine),
+    ).unwrap();
 
-    let mut options: Vec<&str> = vec![") Start", ") Quit"];
+    enable_raw_mode()?;
+
+    let options: Vec<&str> = vec![") Start", ") Quit"];
     let mut selected_option: i32 = 0;
     let max_option_range = options.len() - 1;
-    // transmiter for input
-    let (tx, rx): (mpsc::Sender<bool>, mpsc::Receiver<bool>)      = mpsc::channel();
-    
-    pub fn show_options(options: &Vec<&str>, selected_option: i32) {
-        for (i, option) in options.iter().enumerate() {
-            if i == selected_option.try_into().unwrap() {
-                println!("{} <", option)
-            } else {
-                println!("{}", option);
 
-            }
+    pub fn zone(options: Vec<&str>, selected_option: i32, title: &str) {
+        'zone: loop {
+            show_options(&options, selected_option, title);
+            let event = event::read().unwrap();
         }
     }
-    show_options(&options, selected_option);
+    'master: loop {
+        execute!(stdout, Clear(ClearType::All)).unwrap();
+        // transmiter for input
+        let (tx, rx): (mpsc::Sender<Choice>, mpsc::Receiver<Choice>) = mpsc::channel();
 
-    // thread made to receive input 
-    let input_thread = thread::spawn(move || {
-        // raw mode for receive instantly the user input
-        loop {
-            println!("Waiting for input.");
-            let event = event::read().unwrap();
-            if event == Event::Key(event::KeyCode::Up.into()) {
-                if selected_option > 0 {
-                    tx.send(true).unwrap();
+        show_options(&options, selected_option, "Main Menu\r");
+
+        // cloned for simplicity
+        let slc_opt_copy = selected_option.clone();
+        let max_option_range_copy = max_option_range.clone();
+
+        // thread made to receive input 
+        let _input_thread = std::thread::spawn(move || {
+            loop {
+                let event = event::read().unwrap();
+
+                if event == Event::Key(event::KeyCode::Up.into()) {
+                    if slc_opt_copy > 0 {
+                        execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(3)).unwrap();
+                        tx.send(Choice::Next).expect("Lol 1");
+                        break;
+                    }
+                }
+
+                if event == Event::Key(event::KeyCode::Enter.into()) {
+                    
+                }
+
+                if event == Event::Key(event::KeyCode::Down.into()) {
+                    if slc_opt_copy < max_option_range_copy.try_into().unwrap() {
+                        execute!(io::stdout(), Clear(ClearType::FromCursorUp), cursor::MoveUp(3)).unwrap();
+                        tx.send(Choice::Prev).expect("Lol 2");
+                        break
+                    }                     
+                }
+
+                if event == Event::Key(event::KeyCode::Esc.into()) {
+                    // might add a pop up screen to confirm the exit
+                    terminal::Clear(ClearType::FromCursorUp);
+                    println!("Exiting the listen...\r");
+                    wait_a_sec();
+                    tx.send(Choice::Break).expect("Failed to send Choice::Break");
                     break
                 }
+
             }
-            if event == Event::Key(event::KeyCode::Down.into()) {
-                if selected_option < max_option_range.try_into().unwrap() {
-                    tx.send(false).unwrap();
-                }
-            }
-            if event == Event::Key(event::KeyCode::Esc.into()) {
-                break
-            }
+        });
+
+        let data = rx.recv().unwrap();
+
+        match data {
+            Choice::Next => selected_option -= 1,
+            Choice::Prev => selected_option += 1,
+            Choice::Break => break 'master
+
         }
-        terminal::disable_raw_mode().unwrap();
-    }).join();
 
-    let data = rx.recv().unwrap();
-    if data == true {
-        selected_option -= 1;
-    } else {
-        selected_option += 1;
+
     }
-    for (i, option) in options.iter().enumerate() {
-        if i == selected_option.try_into().unwrap() {
-            println!("{} <", option);
-        } else {
-            println!("{}", option);
-        }
-    }
-    println!("{:?}", data);
+    println!("Disabling Raw Mode\r");
 
-
-
+    execute!(stdout, cursor::Show)?;
     stdout.flush().unwrap();
+
+    disable_raw_mode()
 }
 
 
